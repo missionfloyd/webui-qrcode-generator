b5ba23d86c527be5008dc230154ce5686a9d0526
missionfloyd <missionfloyd@users.noreply.github.com>
2023-06-06 21:41:42 -0600

    Wifi QR code

--- scripts/qrcode.py
+++ scripts/qrcode.py
@@ -6,9 +6,19 @@ import os
 from modules import script_callbacks
 from PIL import Image
 import segno
+from segno import helpers
 
-def generate(data, is_micro):
-    qrcode = segno.make(data, micro=is_micro)
+def generate_text(text, is_micro):
+    qrcode = segno.make(text, micro=is_micro)
+    out = io.BytesIO()
+    qrcode.save(out, scale=5, kind='png')
+    return Image.open(out)
+
+def generate_wifi(ssid, password, security):
+    if security == "None":
+        password = security = None
+
+    qrcode = helpers.make_wifi(ssid=ssid, password=password, security=security)
     out = io.BytesIO()
     qrcode.save(out, scale=5, kind='png')
     return Image.open(out)
@@ -17,13 +27,20 @@ def on_ui_tabs():
     with gr.Blocks() as ui_component:
         with gr.Row():
             with gr.Column():
-                text = gr.Text(label="Text")
-                micro = gr.Checkbox(False, label="Micro")
-                button_generate = gr.Button("Generate", variant="primary")
+                with gr.Tab("Text"):
+                    text = gr.Text(label="Text")
+                    micro_code = gr.Checkbox(False, label="Micro QR Code")
+                    button_generate_text = gr.Button("Generate", variant="primary")
+                with gr.Tab("WiFi"):
+                    ssid = gr.Text(label="SSID")
+                    password = gr.Text(label="Password")
+                    security = gr.Radio(value="None", label="Security", choices=["None", "WEP", "WPA"])
+                    button_generate_wifi = gr.Button("Generate", variant="primary")
             with gr.Column():
                 output = gr.Image(interactive=False).style(height=480)
 
-        button_generate.click(generate, [text, micro], output)
+        button_generate_text.click(generate_text, [text, micro_code], output, show_progress=False)
+        button_generate_wifi.click(generate_wifi, [ssid, password, security], output, show_progress=False)
 
         return [(ui_component, "QR Code", "qrcode_tab")]
 
