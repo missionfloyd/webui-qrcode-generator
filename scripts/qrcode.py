8efc69cbfdab52b721b7cf2f3eb9feaf11c7e9d2
missionfloyd <missionfloyd@users.noreply.github.com>
2023-06-06 21:56:26 -0600

    Geo QR Code

--- scripts/qrcode.py
+++ scripts/qrcode.py
@@ -23,6 +23,12 @@ def generate_wifi(ssid, password, security):
     qrcode.save(out, scale=5, kind='png')
     return Image.open(out)
 
+def generate_geo(latitude, longitude):
+    qrcode = helpers.make_geo_data(latitude, longitude)
+    out = io.BytesIO()
+    qrcode.save(out, scale=5, kind='png')
+    return Image.open(out)
+
 def on_ui_tabs():
     with gr.Blocks() as ui_component:
         with gr.Row():
@@ -36,11 +42,18 @@ def on_ui_tabs():
                     password = gr.Text(label="Password")
                     security = gr.Radio(value="None", label="Security", choices=["None", "WEP", "WPA"])
                     button_generate_wifi = gr.Button("Generate", variant="primary")
+                with gr.Tab("Coordinates"):
+                    with gr.Row():
+                        latitude = gr.Number(0, label="Latitude")
+                        longitude = gr.Number(0, label="Longitude")
+                    button_generate_geo = gr.Button("Generate", variant="primary")
+
             with gr.Column():
-                output = gr.Image(interactive=False).style(height=480)
+                output = gr.Image(interactive=False, show_label=False).style(height=480)
 
         button_generate_text.click(generate_text, [text, micro_code], output, show_progress=False)
         button_generate_wifi.click(generate_wifi, [ssid, password, security], output, show_progress=False)
+        button_generate_geo.click(generate_wifi, [latitude, longitude], output, show_progress=False)
 
         return [(ui_component, "QR Code", "qrcode_tab")]
 
