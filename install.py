5d6054f5c46e6f440c7c8834dec590bd47060162
missionfloyd <missionfloyd@users.noreply.github.com>
2023-11-15 17:40:22 -0700

    Update segno to 1.5.3

--- install.py
+++ install.py
@@ -1,4 +1,17 @@
 import launch
+import importlib.metadata
+import packaging.version
 
-if not launch.is_installed("segno"):
-    launch.run_pip("install segno", "requirements for webui-qrcode-generator")
+def compare_versions(package, version_required):
+    try:
+        version_installed = importlib.metadata.version(package)
+    except Exception:
+        return False
+    
+    if packaging.version.parse(version_installed) < packaging.version.parse(version_required):
+        return False
+    
+    return True
+
+if not compare_versions("segno", "1.5.3"):
+    launch.run_pip('install "segno>=1.5.3"', "requirements for webui-qrcode-generator")
